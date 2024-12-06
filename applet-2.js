7e1b0709f6b526d872d4cbd4d9f5cb388bc4bbd3
Kurt7121 <kurtjastinfabria02@gmail.com>
2024-12-06 20:37:50 +0800

    added initTilelayer

--- applet-2.js
+++ applet-2.js
@@ -27,4 +27,11 @@ class LeafletMap {
         this.btnclear.addEventListener('click', () => this.clearLogs());
 
     }
+    initTileLayer() {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+    }
+
 
