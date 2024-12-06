61e55e35a360e85eb0e52ca92d56f3bdbb6ed4c3
Kurt7121 <kurtjastinfabria02@gmail.com>
2024-12-06 20:43:09 +0800

    added clearlogs

--- applet-2.js
+++ applet-2.js
@@ -59,5 +59,21 @@ class LeafletMap {
             })
             .catch(error => console.error("Error loading servers:", error));
     }
+    clearLogs() {
+        this.attendanceCountTEP = 0;
+        this.attendanceCountCSS = 0;
+        this.attendanceCountBA = 0;
+
+        this.loggedData = [];
+        this.markerCounts = {}; 
+        this.markers.forEach(marker => {
+            const message = marker.getPopup().getContent().split('<br>')[0]; 
+            this.markerCounts[message] = 0;
+            this.updateMarkerPopup(marker, message); 
+        });
+
+        this.updateLogDisplay();
+    }
+    
 
 
