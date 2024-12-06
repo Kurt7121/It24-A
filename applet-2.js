00847914b304430b697d46dd1588e6c2b7e4e5be
Kurt7121 <kurtjastinfabria02@gmail.com>
2024-12-06 20:37:11 +0800

    added documentary

--- applet-2.js
+++ applet-2.js
@@ -7,6 +7,24 @@ class LeafletMap {
         this.attendanceCountTEP = 0;
         this.attendanceCountCSS = 0;
         this.attendanceCountBA = 0;
+
         this.markerCounts = {};
         this.markers = [];
         this.loggedData = []; 
+
+        this.btn = document.getElementById('btn');
+        this.btn1 = document.getElementById('btn1');
+        this.btn2 = document.getElementById('btn2');
+        this.btnclear = document.getElementById('btnclear');
+        this.logCountElement = document.getElementById('logCountTEP');
+        this.logCount1Element = document.getElementById('logCountCCS');
+        this.logCount2Element = document.getElementById('logCountBA');
+        this.idContainer = document.getElementById('logContainer');
+
+        this.btn.addEventListener('click', () => this.dataTEP());
+        this.btn1.addEventListener('click', () => this.dataCSS());
+        this.btn2.addEventListener('click', () => this.dataBA());
+        this.btnclear.addEventListener('click', () => this.clearLogs());
+
+    }
+
