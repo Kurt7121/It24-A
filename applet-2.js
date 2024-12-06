75ae09ee3007653b1d19e8f46e014b1f4d4bdca5
Kurt7121 <kurtjastinfabria02@gmail.com>
2024-12-06 20:35:12 +0800

    added leafletmap

---
+++ applet-2.js
@@ -0,0 +1,9 @@
+class LeafletMap {
+
+    constructor(containerId, center, zoom) {
+        this.map = L.map(containerId).setView(center, zoom);
+        this.initTileLayer();
+        
+        this.attendanceCountTEP = 0;
+        this.attendanceCountCSS = 0;
+        this.attendanceCountBA = 0;
