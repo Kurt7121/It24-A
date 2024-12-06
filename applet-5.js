93f3350dfdf0e29e138ec28e247a66a17f219460
Kurt7121 <kurtjastinfabria02@gmail.com>
2024-12-06 23:26:13 +0800

    added weatherservice

--- applet-5.js
+++ applet-5.js
@@ -29,3 +29,28 @@ class WeatherApp{
         this.weatherCard.style.display = 'block';
     }
 }
+class WeatherService extends WeatherApp {
+    constructor() {
+        super();
+        this.apiKey = '';
+    }
+    async fetchWeather() {
+        this.apiKey = this.APIKeyInput.value.trim(); 
+
+        if (!this.apiKey) {
+            alert('Please enter your API key.');
+            return;
+        }
+        const city = this.cityInput.value;
+        if (city) {
+            const data = await this.getWeatherData(city);
+            if (data) {
+                this.displayWeather(data);
+            } else {
+                alert('City not found. Please try again.');
+            }
+        } else {
+            alert('Please enter a city name.');
+        }
+        
+    }
