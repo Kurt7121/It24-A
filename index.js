ed3098ac3a114792ec31bd81d3756ab07c890e11
Kurt7121 <kurtjastinfabria02@gmail.com>
2024-12-06 23:38:23 +0800

    added cardsearch

---
+++ index.js
@@ -0,0 +1,7 @@
+class CardSearch {
+    constructor(searchInputId, cardContainerId) {
+        this.searchInput = document.getElementById(searchInputId);
+        this.cardContainer = document.getElementById(cardContainerId);
+        this.cards = this.cardContainer.querySelectorAll('.col-md-4');
+        this.init();
+    }
